91e73adc8455f954c23eadc93394e7271f81725a
AnarchistKnight <zhengyang219@gmail.com>
2024-12-22 13:03:24 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #pip config set global.index-url "http://mirrors.cloud.tencent.com/pypi/simple"
 #pip config set global.trusted-host "mirrors.cloud.tencent.com"
 python.exe -m pip install --upgrade pip
-pip install setuptools
-pip install ollama
-pip install pymupdf
-pip install Pillow
-pip install screeninfo
-pip install pyqt6
-pip install tqdm
-pip install psutil
-pip install gputil
+# python --version 3.12.4
+# pip --version 24.3.1
+pip install setuptools==75.6.0
+pip install ollama==0.4.4
+pip install pymupdf==1.25.1
+pip install Pillow==11.0.0
+pip install screeninfo==0.8.1
+pip install pyqt6==6.8.0
+pip install tqdm==4.67.1
+pip install psutil==6.1.1
+pip install gputil==6.1.1
